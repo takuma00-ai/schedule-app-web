8fc55756d3d2f59c851048380a19db7c6178bd31
takuma00-ai <takuma._00icloud.com>
2026-04-14 09:53:48 +0000

    secondコミット

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template,request
-import datetime
+import datetime,json
+
+#Json
+#起動時jsonファイから既存のデータの読み込み
+try:
+    with open("date.json","r") as f:
+        responses=json.load(f)
+except:
+        responses=[]
+
+
 
 app=Flask(__name__)
 
-#名前と希望日をついかする空リスト
-responses=[]
 #てーぶる作成時にしようする日付リスト
 date_list=[
     "4/19","4/20","4/21","4/22","4/23","4/24","4/25","4/26","4/27","4/28","4/29","4/30",
@@ -30,38 +38,50 @@ for d in date_list:
 @app.route("/", methods=["GET", "POST"])
 
 def home():
+    #名前と希望日をついかする空リスト
+    global responses
+
+    best_dates=[]
+    max_count=0
 
     if request.method=="POST":
         name=request.form.get("username")
         selected_dates=request.form.getlist("dates")
+
+        responses=[r for r in responses if r["name"] != name]
 
         responses.append({
             "name":name,
             "dates":selected_dates
         })
 
+            #POST後に保存
+        with open("date.json","w") as f:
+            json.dump(responses,f)
+
         print(responses)
 
-        #人気の日付の集計
-        date_count={d:0 for d in date_list}
+    #人気の日付の集計
+    date_count={d:0 for d in date_list}
 
-        for r in responses:
-            for d in r["dates"]:
-                if d in date_count:
-                    date_count[d]+=1
+    for r in responses:
+        for d in r["dates"]:
+            if d in date_count:
+                date_count[d]+=1
             
-        #最大値の取得
-        max_count=max(date_count.values()) if date_count else 0
-        #一番人気の日付のリスト
-        best_dates=[d for d,c in date_count.items() if c == max_count and c > 0]
+    #最大値の取得
+    max_count=max(date_count.values()) if date_count else 0
+    #一番人気の日付のリスト
+    best_dates=[d for d,c in date_count.items() if c == max_count and c > 0]
+    
 
     return render_template(
         "index.html", 
         responses=responses,
         date_list=date_list,
         date_info=date_info,
-        #best_dates=best_dates,
-        #max_count=max_count,
+        best_dates=best_dates,
+        max_count=max_count,
         )
 
 app.run(debug=True)
